faa328a815d51c7452d6c53590dd26809382f999
Francisco Martínez <fmartinez@pcpractico.es>
2016-03-02 12:13:28 +0100

    WeekTWeets primera versión.
    Primera versión de WeekTWeets

---
+++ README.md
@@ -0,0 +1,94 @@
+WeekTWeets
+==========
+
+(ES) Permite programar el envió de Tweets repetitivos semanalmente.
+(EN) Program allows sending Tweets repetitive weekly.
+
+Versión / Versions
+-------------------
+
+** (ES) Versión Actual es 1.0.0 **
+** (EN) Current version is 1.0.0 **
+
+
+Contributing / Contribución
+---------------------------
+
+(ES) Aceptamos cualquier contribución o bugs detectados.
+(EN) We welcome contributions, be it bug fixes or other improvements. If you fix or change something, please submit a pull request. If you want to report a bug, please open an issue. 
+
+
+Uso / How To
+============
+
+
+(ES) Lo primero es obtener las "Keys" y "Access Tokens" de tu cuenta de Twitter desde https://apps.twitter.com/ y configurar twitter_cfg.json
+(EN) First, needs to retrieve the "Keys And Access Tokens" of your Twitter account, from https://apps.twitter.com/ and configure twitter_cfg.json
+
+Ejemplo / Example : twitter_cfg.json
+
+```Json
+{
+  "AccessToken" : "444516776----myaccesstoken--9B3SZ4uLK6qD7VC8",
+  "AccessSecret" : "Wg1gzUHX----myaccesssecret--dl3k$dkfrtTZ0",
+  "ConsumerKey" : "jNtmt--myconsumerkey--SGafoU30u4z5",
+  "ConsumerSecret" : "edBmc--myconsumersecret--aX3hYk66IvaaJ0yyM3ii"
+}
+
+```
+
+(ES) Seguidamente es necesario configurar las tareas programadas en schedule.json
+(EN) Then you need to configure scheduled tasks in schedule.json
+
+(ES)Campos:
+   dayweek -> Puede especificar las iniciales de los días de la semana que se va a ejecutar "LMXJVSD".
+   time    -> Hora de ejecución ("HH:MM:SS")
+   msg     -> Tweet a enviar (máximo 140 caracteres).
+   last    -> Especifica la última vez que se ha ejecutado, por lo que no se ejecutará más en esa fecha. (DD/MM/AA)
+   count   -> Número de veces que se va a ejecutar. Para que se ejecute de forma indefinida hay que indicar "-1"
+   
+(EN)Fields:
+   dayweek -> Use the initials of Spanish day week to schedule the tweets "LMXJVSD".
+   time    -> Start time ("HH:MM:SS")
+   msg     -> Text to send (max 140 chars).
+   last    -> Specifies the last executed date, so it will not run over on that date. (DD/MM/YY)
+   count   -> Repeat Counter. "-1" For infinite
+
+Ejemplo / Example : twitter_cfg.json
+
+```Json
+{
+  "msgs": [
+    {
+      "dayweek": "LJ",
+      "time": "9:00:00",
+      "msg": "Enviar Tweet 4 veces, los lunes y jueves a las 9H | Send Tweet 4 times, Monday and Thursday at 9 am",
+      "last": "01/01/16",
+      "count": "4"
+    },
+    {
+      "dayweek": "V",
+      "time": "17:00:00",
+      "msg": "Enviar sin límite, cada Viernes a las 17h | Send unlimited , every Friday at 17h",
+      "last": "17/01/16",
+      "count": "-1"
+    }
+  ]
+}
+
+```
+
+(ES) Usar el "Programador de tareas de Windows" para planificar las ejecuciones de WeekTWeets.
+(EN) Use the "windows scheduler" to plan the WeekTWeets executions.
+
+Referencias / References
+========================
+
+NewtonSoft  : http://www.newtonsoft.com/json/help/html/Introduction.htm
+TinyTwitter : https://github.com/jmhdez/TinyTwitter
+ 
+
+Licencia / Licence
+==================
+
+This library is released under the Apache 2.0 licence. See the LICENSE.md file
---
+++ WeekTWeets/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using TinyTwitter_;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Data;
+using System.Net;
+
+
+namespace WeekTWeets
+{
+    public class twitter_cfg
+    {
+        public string AccessToken { get; set; }
+        public string AccessSecret { get; set; }
+        public string ConsumerKey { get; set; }
+        public string ConsumerSecret { get; set; }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("╔══════════════════════════════════════════════════════════════════════╗");
+            Console.WriteLine("║ WeekTweets (Weekly Scheduler Tweets)  || PCpractico.es (2016)        ║");
+            Console.WriteLine("║ Developed by Francisco Mártinez                                      ║");
+            Console.WriteLine("║ Twitter : @pcpracticoes    GitHub: https://github.com/fmartineze     ║");
+            Console.WriteLine("╚══════════════════════════════════════════════════════════════════════╝");
+
+
+            bool Terror = false;
+
+            //Dia de la semana
+            string dia = "";
+            switch (DateTime.Now.DayOfWeek.ToString())
+            {
+                case "Monday": { dia = "L"; break; }
+                case "Tuesday": { dia = "M"; break; }
+                case "Wednesday": { dia = "X"; break; }
+                case "Thursday": { dia = "J"; break; }
+                case "Friday": { dia = "V"; break; }
+                case "Saturday": { dia = "S"; break; }
+                case "Sunday": { dia = "D"; break; }
+            }
+            if (!File.Exists(@Directory.GetCurrentDirectory() + "\\twitter_cfg.json")) {
+                // si no existe el archivo crea uno con los datos esenciales.
+                twitter_cfg cfgfile = new twitter_cfg
+                {
+                    AccessToken = "Twitter AccessToken",
+                    AccessSecret = "Twitter AccessSecret",
+                    ConsumerKey = "Twitter ConsumerKey",
+                    ConsumerSecret = "Twitter ConsumerSecret"
+                };
+                File.WriteAllText(@Directory.GetCurrentDirectory() + "\\twitter_cfg.json", JsonConvert.SerializeObject(cfgfile));
+                Terror = true; }
+
+            if (!File.Exists(@Directory.GetCurrentDirectory() + "\\schedule.json")) {
+                // si no existe el archivo crea uno con los datos demo.
+                string schedule_str = @"{
+   'msgs': [
+    {
+      'dayweek': 'LMXJVSD',
+      'time': '9:00:00',
+      'msg': 'Tweet de prueba / Test Tweet',
+      'last': '01/01/16',
+      'count': '5'
+    },
+    {
+      'dayweek': 'L',
+      'time': '9:00:00',
+      'msg': 'Tweet de prueba / Test Tweet',
+      'last': '02/01/16',
+      'count': '5'
+    },
+  ]
+}";
+                File.WriteAllText(@Directory.GetCurrentDirectory() + "\\schedule.json", schedule_str);
+
+                Terror = true; }
+
+            if (!Terror)
+            {
+                // Leer twitter_cfg.json para obtener las credenciales.
+                JObject Json_settings = JObject.Parse(File.ReadAllText(@Directory.GetCurrentDirectory() + "\\twitter_cfg.json"));
+
+                // Leer Schedule.json para programar los Tweets programados.
+                DataSet dataSet = JsonConvert.DeserializeObject<DataSet>(File.ReadAllText(@Directory.GetCurrentDirectory() + "\\schedule.json"));
+                DataTable dataTable = dataSet.Tables["msgs"];
+
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    if (row["dayweek"].ToString().IndexOf(dia) != -1) // Comparar dia de la semana
+                    {
+                        if (DateTime.Parse(row["time"].ToString()) < DateTime.Now) // Comparar hora de ejecución (>=)
+                        {
+                            string fecha_last = DateTime.Parse(row["last"].ToString()).ToString("dd/MM/yy");
+                            string fecha_now = DateTime.Now.ToString("dd/MM/yy");
+                            if (DateTime.Parse(fecha_last) < DateTime.Parse(fecha_now)) // comparar ultima fecha que se ejecutó
+                            {
+                                if (Int32.Parse(row["count"].ToString()) != 0) // comparar ejecuciones pendientes
+                                {
+                                    Console.WriteLine("- Tweet : [" + row["dayweek"] + "|" + row["time"] + "|" + row["count"] + "] " + row["msg"]);
+                                    Enviar_Tweet(row["msg"].ToString(), Json_settings["AccessToken"].ToString(), Json_settings["AccessSecret"].ToString(), Json_settings["ConsumerKey"].ToString(), Json_settings["ConsumerSecret"].ToString());
+                                    row["last"] = fecha_now; // ;Modificar "last" a date/time actual.
+                                    if (Int32.Parse(row["count"].ToString()) > 0) // Cambiar contador si es > 0
+                                    {
+                                        row["count"] = Int32.Parse(row["count"].ToString()) - 1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+
+                // Serialize Dataset to Json  -> http://www.newtonsoft.com/json/help/html/SerializeDataSet.htm
+                string json = JsonConvert.SerializeObject(dataSet, Formatting.Indented);
+                File.WriteAllText(@Directory.GetCurrentDirectory() + "\\schedule.json", json); // Grabar Cambios
+            } else
+            {
+                Console.WriteLine("- ERROR: Archivos de configuración no encontrados (twitter_cfg.json o schedule.json).");
+                Console.WriteLine("  Se han creado los siguientes archivos:");
+                Console.WriteLine("  twitter_cfg.json    -> Configuración de Token (https://apps.twitter.com/)");
+                Console.WriteLine("  schedule.json       -> Programador de tareas");
+            }
+
+        }
+        static void Enviar_Tweet (string msg_tweet, string AT, string AS, string CK, string CS)
+        {
+            var oauth = new OAuthInfo
+            {
+                AccessToken = AT,
+                AccessSecret = AS,
+                ConsumerKey = CK,
+                ConsumerSecret = CS
+            };
+
+            var twitter = new TinyTwitter(oauth);
+            try
+            {
+                twitter.UpdateStatus(msg_tweet);
+            }
+            catch (WebException ex)
+            {
+                using (var stream = ex.Response.GetResponseStream())
+                using (var reader = new StreamReader(stream))
+                {
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("- ERROR DESCONOCIDO :"+ e);
+            }
+
+        }
+    }   
+}
